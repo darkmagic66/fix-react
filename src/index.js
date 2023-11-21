3305e33ff47bd6f4e99dcb14998f2d260e4d53c7
pongsatorn <owlee.666666@gmail.com>
2023-11-21 16:10:44 +0700

    check

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import StarRating from "./StarRating";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		{/* <StarRating maxRating={5} /> */}
+		<App />
+	</React.StrictMode>
+);
